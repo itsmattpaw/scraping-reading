6549109b627546eb3523e598cfe016ca48cea8b8
itsmattpaw <itsmattpaw@gmail.com>
2022-03-01 11:37:46 -0500

    commit tree 4e8b81a3439ac60e136ace138bfb7de2de83b577

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = URI.open("https://flatironschool.com/")
+#html = URI.open("https://flatironschool.com/")
+#doc = Nokogiri::HTML5(html)
+#puts doc
+#p doc.css(".heading-40-semibold.color-black.mb-20").text
+
+html = URI.open("https://www.havertys.com/furniture/allstores")
+doc2 = Nokogiri::HTML5(html)
+#puts doc
+stores = doc2.css(".storeDetails").css("span")
+stores.each do |location|
+    puts location.text.strip
+end
